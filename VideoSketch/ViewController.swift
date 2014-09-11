df99d02e84d42a6d7878477595029900ed1887cd
Evgeniy Kapralov <evkapral@gmail.com>
2014-09-11 23:36:00 +0400

    Add RenderView (due to odd Swift issues, currently in Objective-C)

--- VideoSketch/ViewController.swift
+++ VideoSketch/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var oglView : RenderView?
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        oglView = RenderView(frame: self.view.bounds)
+        view.addSubview(oglView!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
