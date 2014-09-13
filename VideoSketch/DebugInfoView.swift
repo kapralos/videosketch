aaec25e5c67361d8280ea4bccc0a64f9cf82a594
Evgeniy Kapralov <evkapral@gmail.com>
2014-09-13 13:52:18 +0400

    Implement debug info view

---
+++ VideoSketch/DebugInfoView.swift
@@ -0,0 +1,43 @@
+//
+//  DebugInfoView.swift
+//  VideoSketch
+//
+//  Created by Evgeniy Kapralov on 13/09/14.
+//  Copyright (c) 2014 Kapralos Software. All rights reserved.
+//
+
+import UIKit
+
+class DebugInfoView : UIView
+{
+    var resolutionLabel : UILabel?
+    var fpsLabel : UILabel?
+    
+    required init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+    
+    override required init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        resolutionLabel = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: frame.width, height: frame.height / 2.0))
+        resolutionLabel?.textAlignment = NSTextAlignment.Center
+        resolutionLabel?.textColor = UIColor.grayColor()
+        self.addSubview(resolutionLabel!)
+        
+        fpsLabel = UILabel(frame: CGRect(x: 0.0, y: frame.height / 2.0, width: frame.width, height: frame.height / 2.0))
+        fpsLabel?.textAlignment = NSTextAlignment.Center
+        fpsLabel?.textColor = UIColor.grayColor()
+        self.addSubview(fpsLabel!)
+    }
+    
+    func updateResolution(#width: Int, height: Int)
+    {
+        resolutionLabel?.text = String(width) + " x " + String(height)
+    }
+    
+    func updateFps(#fps: Float)
+    {
+        fpsLabel?.text = String(format: "%.1f", fps) + " fps"
+    }
+}
